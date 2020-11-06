577e717385b3e821048686e3ecab9d7c4a2e8713
Albert Goncalves <goncalves.albert@gmail.com>
2020-11-05 23:48:47 -0500

    first steps

---
+++ src/steps/frag.glsl
@@ -0,0 +1,18 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 RESOLUTION;
+
+float plot(in vec2 st, in float pct) {
+    return smoothstep(pct - 0.02, pct, st.y) -
+        smoothstep(pct, pct + 0.02, st.y);
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy / RESOLUTION;
+    float y = smoothstep(0.0, 0.5, st.x) - smoothstep(0.5, 1.0, st.x);
+    float pct = plot(st, y);
+    vec3 color = ((1.0 - pct) * vec3(y)) + (pct * vec3(1.0, 0.25, 0.75));
+    gl_FragColor = vec4(color, 1.0);
+}
