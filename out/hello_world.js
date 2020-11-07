b9011d1be6959b4d568e33ed8bb1dfbe4c5c9b0b
Albert Goncalves <goncalves.albert@gmail.com>
2020-11-06 20:43:56 -0500

    use distance() fn!

--- out/hello_world.js
+++ out/hello_world.js
@@ -20,7 +20,7 @@ uniform float TIME;
 
 void main() {
     vec2 coord = gl_FragCoord.xy / RESOLUTION;
-    if ((pow(coord.x, 2.0) + pow(coord.y, 2.0)) < 1.0) {
+    if (distance(vec2(0.0), coord) < 1.0) {
         gl_FragColor = vec4(abs(sin(TIME / 3.0)), coord.x, coord.y, 1.0);
     } else {
         gl_FragColor = vec4(0.0);
