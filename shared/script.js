7826180446445a8cb0c412914452534700303976
albertgoncalves <goncalves.albert@gmail.com>
2020-11-06 19:04:48 -0500

    re-factor gluecode

--- shared/script.js
+++ shared/script.js
@@ -2,10 +2,23 @@
 
 "use strict";
 
+var BUFFER_DATA = new Float32Array([
+    -1.0,
+    -1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    1.0,
+]);
+
 function main() {
     var canvas = document.getElementById("canvas");
-    var width = canvas.width;
-    var height = canvas.height;
     var gl = canvas.getContext("webgl");
     var program = gl.createProgram();
     {
@@ -28,48 +41,44 @@ function main() {
         }
         gl.attachShader(program, fragmentShader);
     }
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.error(gl.getProgramInfoLog(program));
-        return;
-    }
-    gl.useProgram(program);
-    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
     {
-        var attribute = gl.getAttribLocation(program, "POSITION");
-        gl.enableVertexAttribArray(attribute);
-        gl.vertexAttribPointer(attribute, 2, gl.FLOAT, false, 0, 0);
+        gl.linkProgram(program);
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            console.error(gl.getProgramInfoLog(program));
+            return;
+        }
+        gl.useProgram(program);
+        gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+        {
+            var attribute = gl.getAttribLocation(program, "POSITION");
+            gl.enableVertexAttribArray(attribute);
+            gl.vertexAttribPointer(attribute, 2, gl.FLOAT, false, 0, 0);
+        }
+        gl.bufferData(gl.ARRAY_BUFFER, BUFFER_DATA, gl.STATIC_DRAW);
     }
-    gl.uniform2f(gl.getUniformLocation(program, "RESOLUTION"), width, height);
-    var data = new Float32Array([
-        -1.0,
-        -1.0,
-        1.0,
-        -1.0,
-        -1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        -1.0,
-        -1.0,
-        1.0,
-    ]);
-    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-    var uniformMouse = gl.getUniformLocation(program, "MOUSE");
-    gl.uniform2f(uniformMouse, width / 2.0, height / 2.0);
+    var width = canvas.width;
+    var height = canvas.height;
+    var mouse = {
+        x: width / 2.0,
+        y: height / 2.0,
+    };
     var box = canvas.getBoundingClientRect();
     function mouseMove(event) {
         var x = event.clientX - box.left;
         var y = event.clientY - box.top;
         if ((0.0 <= x) && (x < width) && (0.0 <= y) && (y < height)) {
-            gl.uniform2f(uniformMouse, x, y);
+            mouse.x = x;
+            mouse.y = height - y;
         }
     }
     window.addEventListener("mousemove", mouseMove, false);
+    var uniformMouse = gl.getUniformLocation(program, "MOUSE");
+    var uniformTime = gl.getUniformLocation(program, "TIME");
+    gl.uniform2f(gl.getUniformLocation(program, "RESOLUTION"), width, height);
     function loop(t) {
         gl.drawArrays(gl.TRIANGLES, 0, 6);
-        gl.uniform1f(gl.getUniformLocation(program, "TIME"), t / 2048.0);
+        gl.uniform1f(uniformTime, t / 2048.0);
+        gl.uniform2f(uniformMouse, mouse.x, mouse.y);
         requestAnimationFrame(loop);
     }
     requestAnimationFrame(loop);
