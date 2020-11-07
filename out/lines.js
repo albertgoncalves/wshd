a2f0985f715df2f4eca09219dcb3366b44bb42a0
Albert Goncalves <goncalves.albert@gmail.com>
2020-11-06 23:17:43 -0500

    hurts my eyes

---
+++ out/lines.js
@@ -0,0 +1,44 @@
+SHADER_VERT = `
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+attribute vec2 POSITION;
+
+void main() {
+    gl_Position = vec4(POSITION, 0.0, 1.0);
+}
+`;
+
+SHADER_FRAG = `
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 RESOLUTION;
+uniform float TIME;
+
+#define PI 3.14159265359
+
+float plot(in float x, in float y, in float k) {
+    return step(x - k, y) - step(x + k, y);
+}
+
+float sin_pos(in float x) {
+    return (sin(x) / 2.0) + 0.5;
+}
+
+float cos_pos(in float x) {
+    return (cos(x) / 2.0) + 0.5;
+}
+
+void main() {
+    vec2 coord = gl_FragCoord.xy / RESOLUTION;
+    float k = (sin_pos(TIME) / 100.0) + 0.02;
+    float x = sin_pos(coord.x * cos_pos(TIME * PI) * PI * 4.0);
+    vec3 color =
+        vec3(sin_pos(coord.x / k), sin_pos(0.75 / k), cos_pos(coord.y / k)) +
+        vec3(plot(x, coord.y, k));
+    gl_FragColor = vec4(color, 1.0);
+}
+`;
---
+++ src/lines/frag.glsl
@@ -0,0 +1,30 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 RESOLUTION;
+uniform float TIME;
+
+#define PI 3.14159265359
+
+float plot(in float x, in float y, in float k) {
+    return step(x - k, y) - step(x + k, y);
+}
+
+float sin_pos(in float x) {
+    return (sin(x) / 2.0) + 0.5;
+}
+
+float cos_pos(in float x) {
+    return (cos(x) / 2.0) + 0.5;
+}
+
+void main() {
+    vec2 coord = gl_FragCoord.xy / RESOLUTION;
+    float k = (sin_pos(TIME) / 100.0) + 0.02;
+    float x = sin_pos(coord.x * cos_pos(TIME * PI) * PI * 4.0);
+    vec3 color =
+        vec3(sin_pos(coord.x / k), sin_pos(0.75 / k), cos_pos(coord.y / k)) +
+        vec3(plot(x, coord.y, k));
+    gl_FragColor = vec4(color, 1.0);
+}
