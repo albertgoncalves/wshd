ae5225833754c71e6bdc704f380a0265541fac01
albertgoncalves <goncalves.albert@gmail.com>
2020-11-06 19:04:26 -0500

    plot circle to highlight axis

--- out/hello_world.js
+++ out/hello_world.js
@@ -19,7 +19,11 @@ uniform vec2 RESOLUTION;
 uniform float TIME;
 
 void main() {
-    vec2 st = gl_FragCoord.xy / RESOLUTION;
-    gl_FragColor = vec4(abs(sin(TIME / 3.0)), st.x, st.y, 1.0);
+    vec2 coord = gl_FragCoord.xy / RESOLUTION;
+    if ((pow(coord.x, 2.0) + pow(coord.y, 2.0)) < 1.0) {
+        gl_FragColor = vec4(abs(sin(TIME / 3.0)), coord.x, coord.y, 1.0);
+    } else {
+        gl_FragColor = vec4(0.0);
+    }
 }
 `;
