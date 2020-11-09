419363c557a3aa54bcaf71a8423605cf3c709312
Albert Goncalves <goncalves.albert@gmail.com>
2020-11-08 19:49:46 -0500

    small clean-up

--- src/02_fractal_brownian_motion/frag.glsl
+++ src/02_fractal_brownian_motion/frag.glsl
@@ -24,7 +24,7 @@ float noise(in vec2 xy) {
     float b = random(i + vec2(1.0, 0.0));
     float c = random(i + vec2(0.0, 1.0));
     float d = random(i + vec2(1.0, 1.0));
-    vec2 u = f * f * (3.0 - (2.0 * f));
+    vec2 u = smoothstep(0.0, 1.0, f);
     return mix(a, b, u.x) + ((c - a) * u.y * (1.0 - u.x)) +
         ((d - b) * u.x * u.y);
 }
@@ -36,11 +36,13 @@ float fbm(in vec2 xy) {
     float a = 0.5;
     vec2 shift = vec2(100.0) + (MOUSE / 1500.0);
     /* NOTE: Rotate to reduce axial bias. */
-    mat2 rot = mat2(cos(0.5), sin(0.5), -sin(0.5), cos(0.5));
+    float s = sin(0.5);
+    float c = cos(0.5);
+    mat2 rot = mat2(c, s, -s, c);
     for (int i = 0; i < N_OCTAVES; ++i) {
         v += a * noise(xy);
-        xy = (rot * xy * 2.0) + shift;
         a *= 0.5;
+        xy = (rot * xy * 2.0) + shift;
     }
     return v;
 }
@@ -48,19 +50,15 @@ float fbm(in vec2 xy) {
 void main() {
     vec2 coord = (gl_FragCoord.xy / RESOLUTION) * 3.0;
     coord += coord * abs(sin(TIME * 0.05) * 3.0);
-    vec3 color = vec3(0.0);
-    vec2 q = vec2(0.0);
-    q.x = fbm(coord + (0.0 * TIME));
-    q.y = fbm(coord + vec2(1.0));
-    vec2 r = vec2(0.0);
-    r.x = fbm(coord + (1.0 * q) + vec2(1.7, 9.2) + (0.15 * TIME));
-    r.y = fbm(coord + (1.0 * q) + vec2(8.3, 2.8) + (0.126 * TIME));
+    vec2 q = vec2(fbm(coord + (0.0 * TIME)), fbm(coord + vec2(1.0)));
+    vec2 r = vec2(fbm(coord + (1.0 * q) + vec2(1.7, 9.2) + (0.15 * TIME)),
+                  fbm(coord + (1.0 * q) + vec2(8.3, 2.8) + (0.126 * TIME)));
     float f = fbm(coord + r);
-    color = mix(vec3(0.101961, 0.619608, 0.666667),
-                vec3(0.666667, 0.666667, 0.498039),
-                clamp((f * f) * 4.0, 0.0, 1.0));
+    vec3 color = mix(vec3(0.101961, 0.619608, 0.666667),
+                     vec3(0.666667, 0.666667, 0.498039),
+                     clamp(f * f * 4.0, 0.0, 1.0));
     color = mix(color, vec3(0.0, 0.0, 0.164706), clamp(length(q), 0.0, 1.0));
-    color = mix(color, vec3(0.666667, 1.0, 1.0), clamp(length(r.x), 0.0, 1.0));
+    color = mix(color, vec3(0.666667, 1.0, 1.0), clamp(r.x, 0.0, 1.0));
     gl_FragColor =
         vec4(((f * f * f) + (0.6 * f * f) + (0.5 * f)) * color, 1.0);
 }
